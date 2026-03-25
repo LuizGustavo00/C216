314009a9e532ba8a13c18575653c7e5f78c4cc8d
LuizGustavo00 <lgcosta1@hotmail.com>
2026-03-24 22:08:24 -0300

    Criacao do programa

---
+++ Main.py
@@ -0,0 +1,32 @@
+from funcoes import menu
+from funcoes import config
+
+def main():
+
+    alunos = []
+    cursos = {}
+
+    
+    des = True
+
+    while des == True:
+        
+        menu.criar()
+        op = input()
+
+        match op:
+            case "0":
+                des = False
+            case "1":
+                menu.cadastrar(alunos,cursos)
+            case "2":
+                menu.mostrarLista(alunos)
+            case "3":
+                #menu.atualizarCadastro()
+                pass
+            case "4":
+                menu.excluirCadastro(alunos)
+                
+
+if __name__ == "__main__":
+    main()
---
+++ funcoes/menu.py
@@ -0,0 +1,66 @@
+def criar():
+    print("Bem vindo ao menu!\n")
+    print("1 - Cadastrar novo aluno")
+    print("2 - Listar alunos")
+    print("3 - Atualizar cadastro")
+    print("4 - Excluir cadastro")
+    print("0 - Sair")
+
+
+
+def cadastrar(alunos,cursos):
+    print("Cadastro de novo aluno\n")
+
+    nome = input("Digite o nome: ")
+    email = input("Digite o email: ")
+    curso = input("Digite o curso: ").upper()
+
+    if curso not in cursos:
+        cursos[curso] = 0
+    
+    cursos[curso] += 1
+
+    aluno = {"nome": nome, "email": email, "curso": curso, "matricula": curso+str(cursos[curso])}
+
+    alunos.append(aluno)
+
+
+def mostrarLista(alunos):
+    if len(alunos) == 0:
+        print("Nenhum aluno cadastrado")
+
+    else:
+        print("Lista de alunos\n")
+
+        for aluno in alunos:
+            print(f"Nome: {aluno['nome']}")
+            print(f"Email: {aluno['email']}")
+            print(f"Curso: {aluno['curso']}")
+            print(f"Matrícula: {aluno['matricula']}")
+            print("-" * 30)
+
+#def atualizarCadastro():
+
+
+def excluirCadastro(alunos):
+    print("Excluir cadastro\n")
+
+    matricula = input("Informe a matricula do aluno: ").upper()
+
+    for aluno in alunos:
+        if aluno["matricula"] == matricula:
+            print("Aluno encontrado")
+            print(f"Nome: {aluno['nome']}")
+            print(f"Matrícula: {aluno['matricula']}")
+
+        conf = input("Confirmar exclusão? (s/n) ").lower()
+
+        if conf == 's':
+            alunos.remove(aluno)
+            print("Aluno excluido com sucesso")
+        elif conf == 'n':
+            print("Exclusão cancelada")
+            print("-" * 10)
+            return
+        else:
+            print("Opção invalida")
